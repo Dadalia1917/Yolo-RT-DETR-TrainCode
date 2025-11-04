1af26ed80616691dddfa6fc06fe33cf65423e64b
Dadalia1917 <2955611455@qq.com>
2025-11-04 14:48:32 +0800

    11th

--- train.py
+++ train.py
@@ -1,27 +1,27 @@
 # coding:utf-8
 import torch
-from ultralytics import YOLO
+from ultralytics import RTDETR
 
 # 模型配置文件
-model_yaml_path = "ultralytics/cfg/models/v11/yolo11.yaml"
+model_yaml_path = "ultralytics/cfg/models/v8/yolov8.yaml"
 # 数据集配置文件
 data_yaml_path = 'datasets/Data/data.yaml'
 # 预训练模型
-pre_model_name = 'yolo11m.pt'
+pre_model_name = 'yolo8n.pt'
 
 if __name__ == '__main__':
     # 检查是否有GPU可用
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # 直接加载预训练模型，而不是先加载yaml再加载权重
-    model = YOLO(pre_model_name).to(device)
+    model = RTDETR(pre_model_name).to(device)
 
     # 训练模型
     results = model.train(data=data_yaml_path,
                           epochs=300,
                           batch=16,  # 批次大小
                           project='runs/detect',  # 指定保存到当前项目的runs/detect目录
-                          name='train_v11',
+                          name='train_v8',
                           workers=10,  # 调整数据加载线程数，匹配14核CPU
                           optimizer='SGD',  # 使用SGD优化器，通常比Adam获得更好的最终精度
                           amp=True)  # 启用自动混合精度训练，加速训练并节省显存
